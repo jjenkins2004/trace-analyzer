c3d75d04d37bcbcd2c678ffe265d7b5432cbfc6e
Joshua Jenkins <2004joshuaj@gmail.com>
2025-07-26 16:17:41 +0300

    build fixes

--- electron/main.ts
+++ electron/main.ts
@@ -21,7 +21,7 @@ process.env.APP_ROOT = path.join(__dirname, "..");
 // 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
 export const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
 export const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
-export const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist");
+export const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist", "renderer");
 
 process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL
   ? path.join(process.env.APP_ROOT, "public")
